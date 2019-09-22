571d6e4eafe42aeb6f7f0cb5ebe6f10c0405a591
pinzonep <pap911@gmail.com>
2019-09-22 09:00:03 +0400

    Add files via upload

---
+++ numberLine/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1569126316,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/player-sheet2.png",
+		"images/numberline-sheet0.png",
+		"images/plus-sheet0.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
